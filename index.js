c84ec2581be242ad6a522a0c4478d36cabc41ebc
QuentinBeauchet <quentinbeauchet@free.fr>
2022-04-13 00:44:43 +0200

    trust proxy

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.enable("trust proxy");
+
 app.use((req, res, next) => {
   res.header("Cross-Origin-Resource-Policy", "cross-origin");
   res.header("Access-Control-Allow-Origin", "*");
